60386e2a172b4d48862fb3d54e5839c5f3c1f17a
nextwebb <oaikhenahpeterson@gmail.com>
2019-12-01 23:49:38 +0100

    submit create product feature implemeneted

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,3 +5,9 @@ exports.createProduct = function(req, res) {
     //res.send("Create new farm Product!")
     res.render("create_product")
 }
+
+exports.submitProduct = function(req, res) {
+    res.send("Created new farm Product!")
+    console.log(res.body)
+    
+}
